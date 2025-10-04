6a1cacde1319d63069d62dd352f5e398087bd232
laggenerator <tomaszgasior30@gmail.com>
2025-10-04 13:27:03 +0000

    Startowy uklad projektu

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+var favicon = require("serve-favicon");
+const PORT = 1789;
+
+app.use(express.static(path.join(__dirname, "public")));
+app.use(favicon(path.join(__dirname, "public/assets", "favicon.png")));
+app.use(express.static(path.join(__dirname, "public/css")));
+app.use(express.static(path.join(__dirname, "public/js")));
+app.use(express.static(path.join(__dirname, "public/assets")));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+app.listen(PORT, () => {
+  console.log(`Biblioteka dziala na http://localhost:${PORT}`);
+});
